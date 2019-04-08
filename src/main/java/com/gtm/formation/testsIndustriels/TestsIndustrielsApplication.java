5fe9d1faf79a05f2b068b4ba3d5172f56e0a4047
ming0601 <fangming0601@gmail.com>
2019-04-08 22:03:23 +0200

    Added all files

---
+++ src/main/java/com/gtm/formation/testsIndustriels/TestsIndustrielsApplication.java
@@ -0,0 +1,13 @@
+package com.gtm.formation.testsIndustriels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsIndustrielsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsIndustrielsApplication.class, args);
+	}
+
+}
